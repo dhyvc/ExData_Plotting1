210de30a720e7429741e8002cd7394ac9952e969
dhyvc <forgithub2014@gmail.com>
2015-02-05 00:09:08 +0200

    EDA Project 1 commit

---
+++ plot1.R
@@ -0,0 +1,6 @@
+power<-read.table("./household_power_consumption.txt", sep=";", skip=66637, nrows=2880)
+headings<-read.table("./household_power_consumption.txt", sep=";", nrows=1, header=T)
+colnames(power)<-colnames(headings)
+png(file="plot1.png")
+hist(power$Global_active_power,main="Global Active Power",xlab="Global Active Power (kilowatts)",col="Red")
+dev.off()
